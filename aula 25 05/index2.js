7e1c498956c8d8503e403392a8b85d1def9da51b
ericpanitz <ericpanitz2004@gmail.com>
2023-04-26 19:51:40 -0300

    Exercicio de operadores

---
+++ aula 25 05/index2.js
@@ -0,0 +1,20 @@
+//Escreva um programa que receba
+//o Pokémon inicial escolhido pela
+//pessoa e imprima no console o seu
+//tipo:
+// Bulbasauro (Planta e Veneno)
+// Charmander (Fogo)
+// Squirtle (Água)
+
+const poke = prompt("escolha seu pokemon inicial: Bulbasauro(Planta e Veneno), Charmander(Fogo) ou Squirtle(Água)") 
+switch (poke){
+    case 'Bulbasauro':
+        Console.log('você escolheu o Bulbasauro como seu pokemon: ')
+        break
+    case 'Charmander':
+        Console.log('você escolheu o Charmander como seu pokemon: ')
+        break
+    case 'Squirtle':
+        Console.log('você escolheu o Squirtle como seu pokemon: ')
+        break 
+}
---
+++ exercicios 26 04/index.js
@@ -0,0 +1,6 @@
+const idade = Number(prompt("Digite sua idade"))
+const idade2 = Number(prompt("Digite a idade de seu melhor amigo"))
+
+
+console.log("Sua idade é maior do que a do seu amigo?")
+console.log(idade >= idade2)
